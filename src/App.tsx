033a70901b08b0f737e1c831a3eaf2823f964c8d
Eryk Mariankowski <dagi12@o2.pl>
2019-08-06 15:56:20 +0200

    redux start

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ACTION_TYPES = {
+  ADD_TODO: 'ADD_TODO',
+  TOGGLE_TODO: 'TOGGLE_TODO',
+  SET_VISIBILITY_FILTER: 'SET_VISIBILITY_FILTER'
+}
+
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+export function addTodo(text: string) {
+  return { type: ACTION_TYPES.ADD_TODO, text }
+}
+
+export function toggleTodo(index: number) {
+  return { type: ACTION_TYPES.TOGGLE_TODO, index }
+}
+
+export function setVisibilityFilter(filter: object) {
+  return { type: ACTION_TYPES.SET_VISIBILITY_FILTER, filter }
+}
+
 const App: React.FC = () => {
   return (
     <div className="App">
---
+++ src/Todo.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react'
+
+interface Props {
+
+}
+
+export const Todo: React.FC<Props> = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+auto rename tag
+auto close tag
